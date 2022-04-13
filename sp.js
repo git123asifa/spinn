4250574e1538975a8e3be1dcf7d1343fea01088c
git123asifa <101800499+git123asifa@users.noreply.github.com>
2022-04-13 21:24:12 +0530

    asifa

---
+++ sp.js
@@ -0,0 +1,10 @@
+var pic1 = document.getElementById("imge");
+function tostart() {
+    pic1.style.animationPlayState = 'running'
+   
+}
+
+function tostop() {
+    pic1.style.animationPlayState="paused";
+
+}
